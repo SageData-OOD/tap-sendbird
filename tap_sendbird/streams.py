c0050d41e55dc468d02497dc14475d5d2005091e
Dimitar Petrunov <dimitar.petrunov@sagedata.net>
2024-11-21 10:48:29 +0200

    refactor: move utility functions in their own module

--- tap_sendbird/streams.py
+++ tap_sendbird/streams.py
@@ -10,37 +10,7 @@ import requests
 from singer_sdk.helpers.jsonpath import extract_jsonpath
 
 from tap_sendbird.client import SendBirdStream
-
-
-def convert_ts_to_milliseconds(ts:int)-> str:
-     assert len(ts) <= 13
-     return ts + (13 - len(ts)) * "0"
-
-def convert_metadata_to_json_string(data: dict | list) -> dict | list:
-    """
-    Recursively converts all dictionary properties named 'metadata' into JSON strings.
-    
-    Args:
-        data (dict, list): The input dictionary or list.
-    
-    Returns:
-        dict or list: The transformed data with 'metadata' fields as JSON strings.
-    """
-    if isinstance(data, dict):
-        # Iterate through dictionary keys
-        for key, value in data.items():
-            if key == "metadata" and isinstance(value, (dict, list)):
-                # Convert "metadata" property to JSON string
-                data[key] = json.dumps(value)
-            else:
-                # Recursively process nested dictionaries or lists
-                data[key] = convert_metadata_to_json_string(value)
-    elif isinstance(data, list):
-        # Iterate through list items
-        for i in range(len(data)):
-            data[i] = convert_metadata_to_json_string(data[i])
-    
-    return data
+from utils import convert_metadata_to_json_string, convert_ts_to_milliseconds_string
 
 
 class UsersStream(SendBirdStream):
@@ -157,12 +127,12 @@ class MessagesStream(SendBirdStream):
         
         """Return URL params that go with the request."""
         if not next_page_token:
-            next_page_token = convert_ts_to_milliseconds(self.get_starting_replication_key_value(context))
+            next_page_token = convert_ts_to_milliseconds_string(self.get_starting_replication_key_value(context))
             
             if context["last_message_ts"] != "-1" and next_page_token > context["last_message_ts"]:
                 self.query_stream = False
 
-        next_page_token = convert_ts_to_milliseconds(next_page_token)
+        next_page_token = convert_ts_to_milliseconds_string(next_page_token)
         self.logger.info("Next Page Token: {}".format(next_page_token))
 
         url_params: dict[str, Any] = {
